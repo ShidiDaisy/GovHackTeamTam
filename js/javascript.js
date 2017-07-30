8b2fcfc09180ff19852081fcb329d9e0b3ff17aa
MarkLi <meng.li6@uq.net.au>
2017-07-30 14:01:45 +1000

    Liming

--- js/javascript.js
+++ js/javascript.js
@@ -45,10 +45,13 @@
         window.dragMoveListener = dragMoveListener;
         
         
+        var saveid = new Array(); 
         
-        
+            var Shenk=0;
         // enable draggables to be dropped into this
         interact('.dropzone').dropzone({
+            
+         
                                        // only accept elements matching this CSS selector
                                        accept: '.draggable',
                                        // Require a 75% element overlap for a drop to be possible
@@ -59,6 +62,7 @@
                                        ondropactivate: function (event) {
                                        // add active dropzone feedback
                                        event.target.classList.add('drop-active');
+                                       
                                        },
                                        ondragenter: function (event) {
                                        var draggableElement = event.relatedTarget,
@@ -74,14 +78,74 @@
                                        event.target.classList.remove('drop-target');
                                        event.relatedTarget.classList.remove('can-drop');
                                        //event.relatedTarget.textContent = 'Dragged out';
+                                          console.log('out the box detected');
+                                          saveid.splice(saveid.indexOf(event.relatedTarget.id),1);
+                                          console.log("final list: ");
+                                        for(var o=0;o<saveid.length;o++){
+                                          console.log(saveid[o]);
+                                            
+                                        }
+                                        Shenk=0;
                                        },
+                                          
                                        ondrop: function (event) {
                                        //event.relatedTarget.textContent = 'Dropped';
-                                       console.log(event.relatedTarget.id);
+                                     //  console.log(event.relatedTarget.id);
+                                      //console.log('pushed ');
+                                            
+                                       for(var i = 0; i< saveid.length; i++){
+                                           if(saveid[i]==event.relatedTarget.id){
+                                                console.log('already in the list');
+                                                    Shenk=1;
+                                           }
+                                           //console.log('pushed1 ');
+                                           
+                                       }
+                                    if(Shenk==0){
+                                            
+                                            saveid.push(event.relatedTarget.id);
+                                          
+                                        }
+                                        for(var o=0;o<saveid.length;o++){
+                                          console.log(saveid[o]);
+                                            
+                                        }
+                                             //console.log('pushed2 ');
+                                             Shenk=0;
+                                      //  console.log('pushed '+event.relatedTarget.id+' to list');
+                                        
                                        },
                                        ondropdeactivate: function (event) {
                                        // remove active dropzone feedback
                                        event.target.classList.remove('drop-active');
                                        event.target.classList.remove('drop-target');
                                        }
-                                       });	
+                                       });
+
+
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+        ['Genre', 'Fantasy & Sci Fi', 'Romance', 'Mystery/Crime', 'General',
+         'Western', 'Literature', { role: 'annotation' } ],
+        ['2010', 10, 24, 20, 32, 18, 5, ''],
+        ['2020', 16, 22, 23, 30, 16, 9, ''],
+        ['2030', 28, 19, 29, 30, 12, 13, '']
+      ]);
+
+        var options = {
+          chart: {
+            title: 'Company Performance',
+            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+          },
+         isStacked: true,
+          bars: 'horizontal' // Required for Material Bar Charts.
+         
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('barchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
